53872507cd5a6697fddcbd6df7b7cb1f7d384f6d
Oleg <olegcuskov@gmail.com>
2024-03-04 13:48:01 +0300

    'Register_simple_tag_for_categories'

--- wood_store/settings.py
+++ wood_store/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "debug_toolbar",
+
     'main',
     'goods',
 ]
@@ -50,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'wood_store.urls'
@@ -119,6 +123,16 @@ STATIC_URL = 'static/'
 # Указываем путь для общей статики
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 # STATIC_ROOT переменная для деплоя
